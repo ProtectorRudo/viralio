fc1abbdaca958faf58583ac88e25982de7c0a2ae
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-03 16:31:16 -0300

    Load rigid Aurora R5 landing fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ import "./viralio-020b-r3-reference.css";
 import "./viralio-020b-r3-compat.css";
 import "./viralio-020b-r4-reference.css";
 import "./viralio-020b-r4-fix.css";
+import "./viralio-020b-r5-layoutfix.css";
 
 export const metadata: Metadata = {
   title: "Viralio · Experiencias que se comparten",
