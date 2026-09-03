6b3b33678a4abe40b6c89f79b8d3355a542c5a73
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-02 22:10:16 -0300

    VIRALIO-009 dynamic merchant experience route

---
+++ src/app/experiencia/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from "next/navigation";
+import { viralio } from "@/application";
+import { MerchantExperience } from "@/ui/merchant-experience";
+
+export const dynamic = "force-dynamic";
+
+export default async function DynamicMerchantExperiencePage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ ref?: string }>;
+}) {
+  const [{ slug }, { ref }] = await Promise.all([params, searchParams]);
+  try {
+    const merchant = await viralio.getMerchantForExperience(slug);
+    return <MerchantExperience merchant={merchant} referralToken={ref} />;
+  } catch {
+    notFound();
+  }
+}
