3997c99c10f3ee7018ddcb31617290f3f539ea1a
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-03 16:20:26 -0300

    Load Aurora reference headline fix last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import "./viralio-020b-r2-motion.css";
 import "./viralio-020b-r3-reference.css";
 import "./viralio-020b-r3-compat.css";
 import "./viralio-020b-r4-reference.css";
+import "./viralio-020b-r4-fix.css";
 
 export const metadata: Metadata = {
   title: "Viralio · Experiencias que se comparten",
